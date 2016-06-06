0f9a825f4a915c3413c802abde189856a2d2207a
lordfriend <lordfriend@outlook.com>
2016-06-06 21:07:33 +0800

    fix the sidebar click issues

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {RouteConfig} from "@angular/router-deprecated";
 import {DefaultComponent} from "./default/default.component";
@@ -6,6 +6,7 @@ import {HomeService} from './home.service';
 import {PlayEpisode} from "./play-episode/play-episode.component";
 import {BangumiDetail} from "./bangumi-detail/bangumi-detail.components";
 import {BangumiList} from "./bangumi-list/bangumi-list.component";
+import {Observable, Subscription} from "rxjs/Rx";
 
 require('./home.less');
 
@@ -22,7 +23,7 @@ const BREAK_POINT = 1330;
   {path: '/bangumi/:bangumi_id', name: 'Bangumi', component: BangumiDetail},
   {path: '/bangumi', name: 'BangumiList', component: BangumiList}
 ])
-export class Home implements OnInit {
+export class Home implements OnInit, OnDestroy {
 
   siteTitle:string = SITE_TITLE;
 
@@ -31,6 +32,9 @@ export class Home implements OnInit {
   sidebarActive: boolean = false;
 
   sidebarOverlap: boolean = false;
+
+  private _sidebarClickSubscription: Subscription;
+  private _resizeSubscription: Subscription;
 
   constructor(titleService:Title, homeService: HomeService) {
     titleService.setTitle(this.siteTitle);
@@ -44,16 +48,49 @@ export class Home implements OnInit {
     })
   }
 
-  toggleSidebar() {
+  toggleSidebar(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
     this.sidebarActive = !this.sidebarActive;
   }
 
-  ngOnInit():any {
+  private checkOverlapMode() {
     let viewportWidth =  window.innerWidth;
     if(viewportWidth <= BREAK_POINT) {
       this.sidebarOverlap = true;
+    } else {
+      this.sidebarOverlap = false;
     }
+  }
+
+  ngOnInit():any {
+    this.checkOverlapMode();
+
+    this._sidebarClickSubscription = Observable.fromEvent(document, 'click')
+      .filter(() => {
+        return this.sidebarOverlap && this.sidebarActive;
+      })
+      .subscribe(
+        () => {
+          this.sidebarActive = false;
+        }
+      );
+
+    this._resizeSubscription = Observable.fromEvent(window, 'resize')
+      .subscribe(
+        () => {
+          this.checkOverlapMode();
+        }
+      );
+
     return null;
   }
 
+
+  ngOnDestroy():any {
+    if(this._sidebarClickSubscription) {
+      this._sidebarClickSubscription.unsubscribe();
+    }
+    return null;
+  }
 }

44504f86f25f349c4aca15c73dbeda865908d470
lordfriend <lordfriend@outlook.com>
2016-09-17 22:34:49 +0800

    add click to close function for test menu in keyword-builder

--- src/app/admin/bangumi-detail/keyword-builder/keyword-builder.component.ts
+++ src/app/admin/bangumi-detail/keyword-builder/keyword-builder.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, OnInit, OnChanges, SimpleChanges, AfterViewInit, OnDestroy} from '@angular/core';
 import {Bangumi} from '../../../entity/bangumi';
 import {FeedService} from './feed.service';
+import {closest} from '../../../../facade/lang';
 
 require('./keyword-builder.less');
 
@@ -9,7 +10,7 @@ require('./keyword-builder.less');
   template: require('./keyword-builder.html'),
   providers: [FeedService]
 })
-export class KeywordBuilder implements OnInit, OnChanges {
+export class KeywordBuilder implements OnInit, OnDestroy, OnChanges, AfterViewInit {
   isEditorOpen: boolean;
 
   @Input() bangumi: Bangumi;
@@ -17,7 +18,10 @@ export class KeywordBuilder implements OnInit, OnChanges {
 
   itemList: {title: string, eps_no: number}[];
 
+  outerClickHandler: (event: MouseEvent) => {};
+
   constructor(private feedService: FeedService) {
+    this.outerClickHandler = this.outerClick.bind(this);
   }
 
 
@@ -25,10 +29,29 @@ export class KeywordBuilder implements OnInit, OnChanges {
     this.isEditorOpen = Boolean(this.bangumi[this.siteName]);
   }
 
+
+  ngOnDestroy(): any {
+    document.body.removeEventListener('click', this.outerClickHandler);
+  }
+
   ngOnChanges(changes: SimpleChanges): any {
-    if(changes['bangumi']) {
+    if (changes['bangumi']) {
       console.log(changes);
       this.isEditorOpen = Boolean(this.bangumi[this.siteName]);
+    }
+  }
+
+  ngAfterViewInit(): any {
+    document.body.addEventListener('click', this.outerClickHandler, true);
+  }
+
+  outerClick(event: MouseEvent) {
+    let target = event.target;
+    console.log(closest(target, '.search-result-wrapper'));
+    if (this.itemList && this.itemList.length > 0 && closest(target, '.search-result-wrapper') === null) {
+      this.itemList = [];
+      event.preventDefault();
+      event.stopPropagation();
     }
   }
 
@@ -43,16 +66,18 @@ export class KeywordBuilder implements OnInit, OnChanges {
 
   testFeed() {
     let keyword = this.bangumi[this.siteName];
-    if(this.siteName === 'dmhy') {
+    if (this.siteName === 'dmhy') {
       this.feedService.queryDmhy(keyword)
         .subscribe((result) => {
           this.itemList = result;
-        }, () => {});
+        }, () => {
+        });
     } else if (this.siteName === 'acg_rip') {
       this.feedService.queryAcgrip(keyword)
         .subscribe((result) => {
           this.itemList = result;
-        }, () => {});
+        }, () => {
+        });
     }
   }
 }

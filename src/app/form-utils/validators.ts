2e8baffe024380ea574ef6861ff88d8863b0822a
lordfriend <lordfriend@outlook.com>
2016-10-07 17:21:41 +0800

    fix some bugs

--- src/app/form-utils/validators.ts
+++ src/app/form-utils/validators.ts
@@ -3,8 +3,8 @@ import {FormGroup} from "@angular/forms";
 
 export function passwordMatch(passwordKey:string, passwordConfirmKey:string) {
   return (group:FormGroup):{[key:string]:any} => {
-    let password = group[passwordKey];
-    let passwordConfirm = group[passwordConfirmKey];
+    let password = group.get(passwordKey);
+    let passwordConfirm = group.get(passwordConfirmKey);
     return password.value !== passwordConfirm.value ? {mismatchedPasswords: true}: null;
   };
 }

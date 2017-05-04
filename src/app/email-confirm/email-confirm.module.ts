87cf4fd92d9331bfb4aa90659f44b8601d5c46ba
lordfriend <lordfriend@outlook.com>
2017-05-04 16:43:04 +0800

    update confirm template, editorconfig

--- src/app/email-confirm/email-confirm.module.ts
+++ src/app/email-confirm/email-confirm.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { EmailConfirm } from './email-confirm.component';
+import { EmailConfirmService } from "./email-confirm.service";
 
 @NgModule({
-    declarations: [EmailConfirm]
+    declarations: [EmailConfirm],
+    imports: [CommonModule],
+    providers: [EmailConfirmService]
 })
 export class EmailConfirmModule {
 

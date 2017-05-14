f1e5f0570d629cb09085e72471531e7ff14f022b
lordfriend <lordfriend@outlook.com>
2017-05-14 14:23:21 +0800

    add some form element to forget pass

--- src/app/forget-pass/forget-pass.component.ts
+++ src/app/forget-pass/forget-pass.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { UIToast, UIToastComponent, UIToastRef } from 'deneb-ui';
+import { UserService } from '../user-service/user.service';
 
 @Component({
     selector: 'forget-pass',
     templateUrl: './forget-pass.html'
 })
 export class ForgetPass {
+    private _subscription = new Subscription();
+    private _toastRef: UIToastRef<UIToastComponent>;
 
+    constructor(private _userService: UserService, toast: UIToast) {
+        this._toastRef = toast.makeText();
+    }
+
+    onSubmit() {
+        // this._userService.resetPassword()
+    }
 }

3d3f4ea274efd3adf361b878d528b4273396f081
lordfriend <lordfriend@outlook.com>
2017-05-25 17:31:04 +0800

    add responsive using IntersectionObserver

--- src/app/responsive-image/responsive-image.directive.ts
+++ src/app/responsive-image/responsive-image.directive.ts
@@ -1,26 +1,33 @@
-import { Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
+import { ResponsiveService } from './responsive.service';
+
+export interface ResponsiveDimension {
+    width: string; // px, rem, em, vw, %, auto
+    height: string; // px, rem, em, vh, %, auto
+}
 
 @Directive({
-    selector: 'img[src]'
+    selector: 'img[responsive]'
 })
 export class ResponsiveImage implements OnInit, OnDestroy {
     private _subscription = new Subscription();
 
     private _src: string;
 
-    constructor(private _element: ElementRef) {
+    constructor(private _element: ElementRef, private _responsiveService: ResponsiveService) {
         console.log('responsive image: #' + (Math.random() * 1000));
         console.log(this._element.nativeElement.style.width);
     }
+
     @Input()
-    set src(url: string) {
+    set responsive(url: string) {
         this._src = url;
         console.log(url);
     }
 
-    get src(): string {
+    @HostBinding() get src(): string {
         return this._src;
     }
 
---
+++ src/app/responsive-image/responsive.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+export interface ObservableStub {
+    target: Element;
+    callback(rect: ClientRect): void;
+}
+
+@Injectable()
+export class ResponsiveService {
+    private _observer: IntersectionObserver;
+
+    private _observableStubList: ObservableStub[] = [];
+
+    constructor() {
+        this._observer = new IntersectionObserver(this.intersectionCallback.bind(this));
+    }
+
+    intersectionCallback(entries: IntersectionObserverEntry[]) {
+        entries.forEach((entry: IntersectionObserverEntry) => {
+            let stub = this.getStub(entry.target);
+            if (stub) {
+                stub.callback(entry.boundingClientRect);
+            }
+        })
+    }
+
+    observe(stub: ObservableStub) {
+        if (this.getStub(stub.target)) {
+            throw new Error('Duplicate ObservableStub on target');
+        }
+        this._observableStubList.push(stub);
+        this._observer.observe(stub.target);
+    }
+
+    unobserve(stub: ObservableStub) {
+        let index = this._observableStubList.findIndex(obStub => obStub == stub);
+        if (index !== -1) {
+            this._observableStubList.splice(index, 1);
+            this._observer.unobserve(stub.target);
+        } else {
+            throw new Error('Cannot find that ObservableStub');
+        }
+    }
+
+    private getStub(target: Element) {
+        if (!this._observableStubList) {
+            return null;
+        }
+        return this._observableStubList.find(stub => stub.target === target);
+    }
+}
